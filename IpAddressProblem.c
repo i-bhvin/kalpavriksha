2d971709f63adb1e89ffa0bc9fae0fc86c0a7776
Bhavin Garg <95097247+i-bhvin@users.noreply.github.com>
2025-01-09 16:26:32 +0530

    Add files via upload

---
+++ IpAddressProblem.c
@@ -0,0 +1,75 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+#include<stdlib.h>
+
+
+int isDigit(char *s){
+    return (*s >= '0' && *s <= '9');
+}
+
+int isDot(char *s){
+    return (*s == '.');
+}
+
+int main() {
+    char input[16];
+    scanf("%s", input);
+    char *str = input;
+    int dotCount = 0;
+    int alreadyInvalid = 0;
+    while(*str){
+        if(isDot(str)){
+            dotCount++;
+            if(dotCount >= 4){
+                alreadyInvalid = 1;
+                break;
+            }
+            if(*(str + 1) == '\0' || *(str + 1) == '.'){
+                alreadyInvalid = 1;
+                break;
+            }
+            
+            str++;
+        }
+        else if(isDigit(str)){
+            
+            if(*str == '0' && *(str + 1) != '\0' && *(str + 1) != '.'){
+                alreadyInvalid = 1;
+                break;
+            }
+            
+            
+            int num = 0;
+            while(isDigit(str)){
+                num = num*10 + ((*str) - '0');
+                str++;
+            }
+            str--;
+            
+            if(num < 0 || num > 255){
+                alreadyInvalid = 1;
+                break;
+            }
+        }
+        else {
+            alreadyInvalid = 1;
+            break;
+        }
+        str++;
+    }
+    if(alreadyInvalid){
+        printf("Invalid");
+        alreadyInvalid = 1;
+    }
+    
+    if(alreadyInvalid == 0 && dotCount < 3){
+        printf("Invalid");
+        alreadyInvalid = 1;
+    } 
+    
+    if(alreadyInvalid == 0 && *str == '\0') printf("Valid");
+    
+    
+
+    return 0;
+}
