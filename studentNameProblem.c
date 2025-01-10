53432dbf20aa27015b793b8c77c79727905f454c
Bhavin Garg <95097247+i-bhvin@users.noreply.github.com>
2025-01-10 10:12:34 +0530

    Update studentNameProblem.c
    Added modularity to the code

--- studentNameProblem.c
+++ studentNameProblem.c
@@ -2,6 +2,53 @@
 #include <stdlib.h>
 #include <string.h>
 
+void freeSpace(char ***names, int row, int col){
+    for (int i = 0; i < row; i++){
+        for (int j = 0; j < col; j++){
+            free(names[i][j]);
+        }
+        free(names[i]);
+    }
+    free(names); 
+}
+
+void handleInput(char ***names, int row, int col, int *vowelCount, int *longestNameLength, char **longestName){
+    
+    printf("Enter the names:\n");
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            printf("Name at (%d, %d):", i, j);
+            scanf("%s", names[i][j]);
+
+            if ( names[i][j][0] == 'A' || names[i][j][0] == 'E' || names[i][j][0] == 'I' || names[i][j][0] == 'O' || names[i][j][0] == 'U' || names[i][j][0] == 'a' || names[i][j][0] == 'e' || names[i][j][0] == 'i' || names[i][j][0] == 'o' || names[i][j][0] == 'u'){
+                (*vowelCount)++;
+            }
+
+            if (strlen(names[i][j]) > *longestNameLength)
+            {
+                *longestNameLength = strlen(names[i][j]);
+                *longestName = names[i][j];
+            }
+        }
+    }
+}
+
+void printNames(char ***names, int row, int col){
+    printf("The 2D array of names is:\n");
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            printf("%s ", names[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 int main()
 {
 
@@ -25,49 +72,16 @@ int main()
     int vowelCount = 0, longestNameLength = 0;
     char *longestName = NULL;
 
-    printf("Enter the names:\n");
+    handleInput(names, row, col, &vowelCount, &longestNameLength, &longestName);
+    
+    printNames(names, row, col);
 
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
-            printf("Name at (%d, %d):", i, j);
-            scanf("%s", names[i][j]);
-
-            if ( names[i][j][0] == 'A' || names[i][j][0] == 'E' || names[i][j][0] == 'I' || names[i][j][0] == 'O' || names[i][j][0] == 'U' || names[i][j][0] == 'a' || names[i][j][0] == 'e' || names[i][j][0] == 'i' || names[i][j][0] == 'o' || names[i][j][0] == 'u'){
-                vowelCount++;
-            }
-
-            if (strlen(names[i][j]) > longestNameLength)
-            {
-                longestNameLength = strlen(names[i][j]);
-                longestName = names[i][j];
-            }
-        }
-    }
-
-    printf("The 2D array of names is:\n");
-
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
-            printf("%s ", names[i][j]);
-        }
-        printf("\n");
-    }
+    
 
     printf("The number of names starting with a vowel:%d\n", vowelCount);
     printf("The longest name:%s", longestName);
 
-    for (int i = 0; i < row; i++){
-        for (int j = 0; j < col; j++){
-            free(names[i][j]);
-        }
-        free(names[i]);
-    }
-
-    free(names); 
+    freeSpace(names, row, col);
 
     return 0;
 }
