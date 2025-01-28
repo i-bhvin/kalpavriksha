f19036d37de67039c3a7c97957fe559228b40d1c
Bhavin Garg <95097247+i-bhvin@users.noreply.github.com>
2025-01-28 14:52:23 +0530

    Update twoPointerSearch.c

--- searching/twoPointerSearch.c
+++ searching/twoPointerSearch.c
@@ -8,8 +8,22 @@ Given an array of distinct integers and a target sum, find all pairs of elements
 #include <stdio.h>
 #include <stdlib.h>
 
+
+void bubbleSort(int *arr, int n) {
+    int i, j, temp;
+    for (i = 0; i < n-1; i++) {
+        for (j = 0; j < n-i-1; j++) {
+            if (arr[j] > arr[j+1]) {
+                swap(&arr[j], &arr[j+1]);
+            }
+        }
+    }
+}
+
 void twoSumPairs(int *arr, int n, int target)
 {
+
+    bubbleSort(arr, n);
     int start = 0, end = n - 1;
     int isPairFound = 0;
 
